fd87a390644700d8f8eece7692c07117c27efdd3
ApriZal <aprizalrizalzal@gmail.com>
2021-10-06 20:31:18 +0800

    update_gradle

--- app/src/androidTest/java/me/aprizal/githubusers/main/MainActivityTest.java
+++ app/src/androidTest/java/me/aprizal/githubusers/main/MainActivityTest.java
@@ -9,7 +9,6 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.view.KeyEvent;
 
